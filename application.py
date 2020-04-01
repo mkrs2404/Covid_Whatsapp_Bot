dcd71827d94ac6e98586dc33d5be74c40eadd72e
mkrs2404 <kumarmohit2496@gmail.com>
2020-04-02 01:04:47 +0530

    First commit. Auto update on total count every 3 hours.

---
+++ application.py
@@ -0,0 +1,101 @@
+from flask import Flask, request
+from twilio.twiml.messaging_response import MessagingResponse
+from covid_bot import load
+from twilio.rest import Client
+import logging
+import threading
+import time
+from datetime import datetime
+import covid_bot
+
+def f_write(text:str):
+    f = open("log.txt", 'a+')
+    f.write(str(text)+"\n")
+
+lock = threading.Lock()
+FORMAT = '[%(asctime)-15s] %(message)s'
+logging.basicConfig(format=FORMAT, level=logging.WARNING, filename = 'corona_bot.log', filemode = 'a')
+account_sid = 'ACf3ac76821ea9d7b771cf2683be44df27'
+auth_token = '62488e676c48108c9cb2a9b10e7f2446'
+client = Client(account_sid, auth_token)
+
+application = Flask(__name__)
+
+@application.route('/bot', methods=['POST'])
+
+def bot():
+    state_ut_list = {'Andhra Pradesh': 1, 'Arunachal Pradesh': 2, 'Assam': 3, 'Bihar': 4, 'Chhattisgarh': 5, 'Goa': 6, 'Gujarat': 7, 'Haryana': 8, 'Himachal Pradesh': 9, 'Jharkhand': 10, 'Karnataka': 11, 'Kerala': 12, 'Madhya Pradesh': 13, 'Maharashtra': 14, 'Manipur': 15, 'Meghalaya': 16, 'Mizoram': 17, 'Nagaland': 18, 'Odisha': 19, 'Punjab': 20, 'Rajasthan': 21, 'Sikkim': 22, 'Tamil Nadu': 23, 'Telangana': 24, 'Tripura': 25, 'Uttar Pradesh': 26, 'Uttarakhand': 27, 'West Bengal': 28, 'Andaman and Nicobar Islands': 29, 'Chandigarh': 30, 'Dadra and Nagar Haveli': 31, 'Daman and Diu': 32, 'Delhi': 33, 'Jammu and Kashmir': 34, 'Ladakh': 35, 'Lakshadweep': 36, 'Puducherry': 37, 'Total': 38}
+    data = covid_bot.corona_bot()
+    incoming_msg = request.values.get('Body', '').lower()
+    incoming_user = request.values.get('From')
+    logging.critical(f"{incoming_user} : {incoming_msg}")
+    resp = MessagingResponse()
+    msg = resp.message()
+
+    cur_data = load()
+
+    message = ''
+    key_list = list(state_ut_list.keys()) 
+    val_list = list(state_ut_list.values())
+    try:
+        if 'hi' in incoming_msg:
+            #sending the list of states
+            message = f"*Which State's/UT's data would you like to know?* \n\n"
+            for state in state_ut_list:
+                message += f"{state_ut_list[state]}. {state} \n\n"
+            message += "\n*Type the number.* \n\n_The data shown is not official. It is collected from state press bulletins and official handles and reliable news channels._ "
+
+        elif incoming_msg.isdigit() and int(incoming_msg) in range(1,39):
+            state = key_list[val_list.index(int(incoming_msg))]
+            if state in cur_data and cur_data[state][0] != '0':
+                message = f'*Data for {state}*.\n\nTotal Confirmed cases  = {cur_data[state][0]} \nCured/Discharged/Migrated = {cur_data[state][1]} \nPossible Deaths = {cur_data[state][2]}\n\n_Stay at home. Stay safe._'
+            else:
+                message = f'No cases yet in {state}.'
+
+        else:
+            message = 'Please provide correct input.'
+ 
+        msg.body(message)
+    
+    except Exception as e:
+        logging.exception(f"Some Failure in the  bot Script!! : {e}")
+    
+    return(str(resp))
+
+
+def searchUpdates():
+    try:
+        while(True):
+            message_body = ''
+            bot_data = covid_bot.corona_bot()
+            change_in_data = bot_data["changed"]
+            message = bot_data['message']
+            users = set()
+            if change_in_data == True:
+                client_data = client.messages.list()             
+                for msg in client_data:
+                    if msg.to != 'whatsapp:+14155238886':
+                        users.add(msg.to)
+
+                for msg in message:
+                    message_body += msg
+
+                for user in users:
+                    client.messages.create(body=message_body,from_='whatsapp:+14155238886',to=user)
+                    logging.warning(f"Message sent to {user}")
+                    time.sleep(1)
+                 
+                # client.messages.create(body=message_body,from_='whatsapp:+14155238886',to='whatsapp:+917004784338')
+                # time.sleep(1)
+                logging.warning("All Messages sent to users")
+            time.sleep(3600 * 3)
+    except Exception as e:
+        logging.warning(e)
+
+if __name__ == "__main__":
+    search_updates = threading.Thread(target=searchUpdates)
+    search_updates.start()
+    application.env='production'
+    application.run(debug = True, host='0.0.0.0',port='5000',use_reloader=False)
+    search_updates.join()
+    
---
+++ covid_bot.py
@@ -0,0 +1,92 @@
+import json
+import requests
+import time
+import datetime
+import logging
+import threading
+from json import JSONDecodeError
+
+
+lock = threading.Lock()
+
+FILE_NAME = 'corona_data.json'
+
+def f_write(text:str):
+    f = open("log.txt", 'a+')
+    f.write(str(text)+"\n")
+
+def save(x):
+    with open(FILE_NAME, 'w') as f:
+        json.dump(x, f)
+    
+def load():
+    res = {}
+    with open(FILE_NAME, 'r') as f:
+        try:
+            res = json.load(f)
+        except JSONDecodeError:
+            pass
+    return res
+
+URL = 'https://api.covid19india.org/data.json'
+
+FORMAT = '[%(asctime)-15s] %(message)s'
+
+logging.basicConfig(format=FORMAT, level=logging.WARNING, filename = 'corona_bot.log', filemode = 'a+')
+
+def corona_bot():  
+    cur_data = []
+    info = []
+    message = []
+    any_change = False
+    try:
+        past_data = load()
+        response = requests.get(URL)
+        jsonData = response.json()
+        state_wise_data = jsonData['statewise']
+        info = []
+        for state in state_wise_data:
+            changed = False
+            current_state = state['state']
+            cur_data = [state['confirmed'], state['recovered'], state['deaths']]
+            
+            if current_state not in past_data:
+                past_data[current_state] = {}
+                changed = True
+                any_change = True
+
+            else:
+                if past_data[current_state] != cur_data:
+                    changed = True
+                    any_change = True
+                    if past_data[current_state][0] == 0:
+                        data = f"*New case/cases appeared in {current_state}*: \n\nConfirmed cases = {state['confirmed']} \nCured/Discharged/Migrated = {state['recovered']}\nPossible Deaths = {state['deaths']}\n\n\n"
+                        info.append(data)
+                        message.append(data)
+
+                    else:
+                        data = f"*Change in data of {current_state}*: \n\nConfirmed cases = {past_data[current_state][0]} --> {state['confirmed']} \nCured/Discharged/Migrated = {past_data[current_state][1]} --> {state['recovered']}\nPossible Deaths = {past_data[current_state][2]} --> {state['deaths']}\n\n\n"
+                        info.append(data)
+                        if current_state == 'Total':
+                            message.append(data)
+
+            if changed == True:
+                past_data[current_state] = cur_data
+                
+        bot_data = {"changed": any_change,"message":message}
+
+        if any_change == True:
+            lock.acquire()
+            save(past_data)
+            lock.release()
+
+        for event in info:
+            logging.critical(event)
+
+    except Exception as e:
+        logging.exception(f"Some Failure in the handler Script!! : {e}")
+
+    return bot_data
+
+if __name__ == "__main__":
+    corona_bot()
